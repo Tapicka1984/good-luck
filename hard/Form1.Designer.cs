c01a12b81c96efd8faec45de03b63dfd5dd7ee3c
the Tap <97515458+Tapicka1984@users.noreply.github.com>
2023-04-12 09:34:20 +0200

    Add files via upload

---
+++ hard/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace hard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 501);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Zápis známky";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Ondra Zelený",
+            "Jakub Volema",
+            "Marti Sýrový",
+            "Hynek Svobody ",
+            "Pavel Štulhofer",
+            "Tomáš Ružička",
+            "Honza Šerminátor",
+            "Štonda Tolfa"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 340);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(138, 474);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Váha";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(138, 434);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Známka";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(141, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 64);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Výpis známek";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 6);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 225);
+            this.listBox1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(138, 390);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Pocet znamek";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 387);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 434);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 474);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(139, 501);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(144, 26);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "oddelit čárkou, nedelat forky,\r\n jinak to spadne";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1 Ondra Zelený",
+            "2 Jakub Volema",
+            "3 Marti Sýrový",
+            "4 Hynek Svobody ",
+            "5 Pavel Štulhofer",
+            "6 Tomáš Ružička",
+            "7 Honza Šerminátor",
+            "8 Štonda Tolfa"});
+            this.comboBox2.Location = new System.Drawing.Point(141, 148);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(141, 106);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 39);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Zvolte jmeno zaka\r\nkdyž budete mit stesti\r\ntak se to vypíše";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(268, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 225);
+            this.listBox2.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 577);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ hard/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*1. Ondra Zelený
+        2. Jakub Volema
+        3. Marti Sýrový
+        4. Hynek Svobody 
+        5. Pavel Štulhofer
+        6. Tomáš Ružička
+        7. Honza Šerminátor
+        8. Štonda Tolfa*/
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.Text != string.Empty)
+                {
+                    FileStream data = new FileStream("znamky.dat", FileMode.OpenOrCreate, FileAccess.Write);
+                    BinaryWriter zapis = new BinaryWriter(data, Encoding.GetEncoding("windows-1250"));
+                    string zaznam = (comboBox1.Text + "." + textBox2.Text + "." + textBox4.Text + "." + textBox3.Text + "|");
+                    zapis.Write(zaznam);
+                    zapis.Close();
+                    data.Close();
+                }
+                else
+                {
+                    MessageBox.Show("vyber neco");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("ERROR!!!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileStream data = new FileStream("znamky.dat", FileMode.Open, FileAccess.Read);
+            BinaryReader cteni = new BinaryReader(data, Encoding.GetEncoding("windows-1250"));
+            data.Position = 0;
+            char[] pole = (comboBox2.Text).ToCharArray();
+            int cislo1 = pole[1] - 48;
+            while (data.Position < data.Length)
+            {
+                string text = cteni.ReadString();
+                char[] pole2 = text.ToCharArray();
+                int cislo2 = pole2[1] - 48;
+                if (cislo1 == cislo2)
+                {
+                    string[] pole3 = text.Split('.');
+                    listBox1.Items.Add(pole3[0]);
+                    listBox2.Items.Add("znamky: " + pole3[3] + " vahy:" + pole[2]);
+                    textBox1.Text = pole3[0];
+
+                }
+            }
+            data.Close();
+            cteni.Close();
+        }
+    }
+}
